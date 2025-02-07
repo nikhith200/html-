c9e7041412b8370c23cab9f376d63b1f53138e44
nikhith200 <22c11a0591@anurag.ac.in>
2025-02-07 14:12:33 +0530

    hello world
    node js

---
+++ lilly.js
@@ -0,0 +1,9 @@
+//variables
+//Menu->Run->Run With Out Debugging
+
+let message="Hello,World";
+const PI=3.14;
+//Function
+function greet(name){
+    return 'Hello,${name}!';
+}
---
+++ nikhitha.js
@@ -0,0 +1,4 @@
+function greet(name){
+    return `hello, ${name}`;
+}
+console.log(greet("nikitha 123"));
